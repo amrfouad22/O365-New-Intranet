a16b8aff00bf5df317ab1a5c036a28c53c376bcd
Amr Fouad <amr.fouad@hotmail.co.uk>
2016-01-27 11:57:08 +1100

    add redis cache

--- imageHandler.js
+++ imageHandler.js
@@ -1,37 +1,60 @@
 var express = require('express');
 var request=require('request');
-var base64=require('base64id').Base64;
+var base64=require('base64-js');
+var redis=require('redis');
 var router = express.Router();
 
 router.post('/', function(req, res) {
-    //read the request url and try to get it from mongodb, if not try Office365 and put it in mongodb for further usage.
+    //read the request url and try to get it from redis cache, if not try Office365 and put it in mongodb for further usage.
     var url=req.body.image;
-    
-    
-    //get the item from SPO using same access token submitted by AngularJS SPA
-    var weburl='https://insightme.sharepoint.com/sites/pub/news';
-    //try to get it from cache first
-    //get the image url and get the list folder
-    url=url.replace(weburl,'');
-    var index=url.lastIndexOf('/');
-    var folder=url.substring(1,index);
-    var file=url.substring(index+1);
-    var fileUrl=weburl+"/_api/web/GetFolderByServerRelativeUrl('"+folder+"')/Files('"+file+"')/$value";
-    var options = {
-        url: fileUrl,
-        headers: {
-            'authorization': req.body.token
-        }
-    };
-    //get file data from SharePoint online
-    request.get(options,function(error,response,body){
-        if (!error && response.statusCode == 200) {
-            res.send(base64.encode(response.data));
-        } 
-        else{
-            console.log("can't get SPO file..")
-            res.sendStatus(403);
-        }
+    //create redis client
+    var client = redis.createClient();
+    client.on('connect',function(){  
+        //try to get the value from redis  
+        client.get(req.body.image,function(err,reply){
+            if(err){
+                res.sendStatus(500);
+                return;
+            }
+            //value doesn't exists , try to get it from SPO
+            if(!reply)
+            {
+                console.log(err);
+                //get the item from SPO using same access token submitted by AngularJS SPA
+                var weburl='https://insightme.sharepoint.com/sites/pub/news';
+                //try to get it from cache first
+                //get the image url and get the list folder
+                url=url.replace(weburl,'');
+                var index=url.lastIndexOf('/');
+                var folder=url.substring(1,index);
+                var file=url.substring(index+1);
+                var fileUrl=weburl+"/_api/web/GetFolderByServerRelativeUrl('"+folder+"')/Files('"+file+"')/$value";
+                var options = {
+                    url: fileUrl,
+                    headers: {
+                        'authorization': req.body.token
+                    }
+                };
+                //get file data from SharePoint online
+                request.get(options,function(error,response,body){
+                    if (!error && response.statusCode == 200) {
+                        var result=base64.fromByteArray(response.body);
+                        //save the result to redis cache
+                        client.set(req.body.image,result);
+                        res.send(result);
+                    } 
+                    else{
+                        console.log("can't get SPO file..")
+                        res.sendStatus(403);
+                    }
+                });
+            }
+            //if the value is retrieved send the reply
+            else{
+                res.send(reply);
+            }
+        });
+       
     });
 });
 
